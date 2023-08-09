3ee55515806a4fa4584c8c6292ea9a7c82ad3ccf
Gommzy <gommzy@gmx.de>
2023-08-09 16:22:03 +0200

    improve readability of project information component

--- src/pages/home/projects/information/index.tsx
+++ src/pages/home/projects/information/index.tsx
@@ -5,7 +5,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faTimes} from "@fortawesome/free-solid-svg-icons";
 
 export default function ProjectInformation({project, close}: { project: ProjectData, close: () => void }) {
-    return <div className={"md:p-6 overflow-x-hidden"}>
+    return <div className={"md:p-6 overflow-x-hidden max-w-2xl m-auto"}>
         <div className={"p-6 relative"}>
             <div className={"flex justify-between items-center"}>
                 <div className={"w-6 h-6 block md:hidden"}/>
