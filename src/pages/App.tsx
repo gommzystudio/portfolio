31e0b76aa8075382140a6fd0d3e8ebaf4ebb9d9c
Gommzy <gommzy@gmx.de>
2023-08-09 16:47:34 +0200

    Init Vercel

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,10 @@
 import Home from "./home";
+import React from "react";
+import {Analytics} from '@vercel/analytics/react';
 
 export default function App() {
-    return <Home/>;
+    return <>
+        <Home/>
+        <Analytics/>
+    </>;
 }
